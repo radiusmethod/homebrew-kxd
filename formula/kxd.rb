225d2ed64f9a6710f2c633172dc42c1e2e3e4b64
Danny Gershman <danny@radiusmethod.com>
2023-09-22 13:09:36 -0400

    bumping to v0.0.2

--- formula/kxd.rb
+++ formula/kxd.rb
@@ -1,9 +1,9 @@
 class Kxd < Formula
-    ver = "0.0.1"
+    ver = "0.0.2"
     desc "Kubeconfig Switcher in Go"
     homepage "https://github.com/radiusmethod/kxd"
-    url "https://github.com/radiusmethod/kxd/archive/v0.0.1.tar.gz"
-    sha256 "ff51aa5d4545d046d86330d0eb1ba60ab65e398286bde0013bfe4d743b2a734a"
+    url "https://github.com/radiusmethod/kxd/archive/v0.0.2.tar.gz"
+    sha256 "878e388620d11b820579a146651ca6dea558638c06531083df5cf36ede08f096"
     license "MIT"
     head "https://github.com/radiusmethod/kxd.git", branch: "main", :tag => ver
     version ver
