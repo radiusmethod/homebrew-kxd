f2edc99a8d1b349f5d4705e3c0d666388c3b9c87
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2023-09-25 10:50:50 -0400

    Update kxd.rb

--- Formula/kxd.rb
+++ Formula/kxd.rb
@@ -3,7 +3,7 @@ class Kxd < Formula
     desc "Kubeconfig Switcher in Go"
     homepage "https://github.com/radiusmethod/kxd"
     url "https://github.com/radiusmethod/kxd/archive/v0.0.4.tar.gz"
-    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+    sha256 "b71380e300853900b7e619fe4aaebd603d78d1ca38fc9698ce3c4aedaa20a12c"
     license "MIT"
     head "https://github.com/radiusmethod/kxd.git", branch: "main", :tag => ver
     version ver
